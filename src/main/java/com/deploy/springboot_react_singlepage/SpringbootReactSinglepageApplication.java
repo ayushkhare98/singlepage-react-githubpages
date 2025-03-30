2acf6ef9e52253076bffdbf6b365d3fd33446745
ayushkhare98 <ayushkhare98@gmail.com>
2025-03-30 13:36:37 -0700

    commitone

---
+++ src/main/java/com/deploy/springboot_react_singlepage/SpringbootReactSinglepageApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.springboot_react_singlepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactSinglepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactSinglepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/springboot_react_singlepage/SpringbootReactSinglepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.springboot_react_singlepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactSinglepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
